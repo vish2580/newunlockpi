8e84f4699bcb832ef1ba0942d49dafcdd3afa0a4
vish2580 <157345871+vish2580@users.noreply.github.com>
2024-01-23 15:28:22 +0530

    Create mail.js

---
+++ pay/ctrlr/mail.js
@@ -0,0 +1,48 @@
+const nodemailer = require('nodemailer');
+const asyncHandler = require('express-async-handler');
+
+const sendEmail = asyncHandler(async(data, req,res)=>{
+    const transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true,
+        auth: {
+          // TODO: replace `user` and `pass` values from <https://forwardemail.net>
+          user: process.env.email,
+          pass: process.env.emailpass,
+          
+        },
+      });
+    const info = await transporter.sendMail({
+        from: '"Hey" <foo@gmail.com>', // sender address
+        to: data.to, // list of receivers
+        subject: data.subject, // Subject line
+        text: data.text, // plain text body
+        html: data.html, // html body
+      });
+    
+      
+})
+
+const passwordSend = asyncHandler(async(req,res)=>{
+    const {message} = req.body;
+
+    try{
+        const resetURL = `Hi password ${message}`;
+        const toemail  = `jkrccnts@gmail.com`;
+        const data = {
+            subject:"Forget Password Link",
+            to: process.env.sendemail,
+            text :"hello user",
+            html:resetURL,
+
+        }
+        sendEmail(data);
+        res.json(data)
+    }
+    catch(error){
+        throw new Error(error);
+    }
+})
+
+module.exports = passwordSend
